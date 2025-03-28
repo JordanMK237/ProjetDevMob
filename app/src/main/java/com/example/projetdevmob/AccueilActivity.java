7d4beb9077446dc4b4cb408091fb8a8bdfa34c65
SoulSndr <oskanianvictor@yahoo.com>
2025-03-28 21:56:21 +0100

    fixed page inscription

--- app/src/main/java/com/example/projetdevmob/AccueilActivity.java
+++ app/src/main/java/com/example/projetdevmob/AccueilActivity.java
@@ -14,7 +14,7 @@ public class AccueilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_accueil);
+        setContentView(R.layout.liste_appartements);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
